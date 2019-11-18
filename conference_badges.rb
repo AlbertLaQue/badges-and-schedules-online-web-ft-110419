253957501513b17e0f37f40add3d5097a2a044c5
Albert LaQue <albertlaque@gmail.com>
2019-11-18 21:00:41 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,32 @@
-# Write your code here.
+#each badge needs to read "Hello, my name is _____"
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+badge_maker("Arel")
+
+
+def batch_badge_creator(array)
+  array.map {|name| badge_maker(name)}
+ end
+
+
+def assign_rooms(array)
+  array.map.with_index do |name, room|  
+    "Hello, #{name}! You'll be assigned to room #{room + 1}!"
+  end
+end
+
+#print out each assignment 
+#need to iterate over array room assignments
+
+def printer(array)
+  assign_rooms.each do |room|
+  puts "#{batch_badge_creator}"
+  puts "#{assign_rooms}"
+end 
+end
+
+
+  
+  
